1bd57b3214e5a0eeefbf7da856da3dd1977c8196
Jason Lukose <jlukose12@gmail.com>
2018-04-27 01:31:25 -0400

    Some basic skeleton ChordNode

---
+++ ChordNode.py
@@ -0,0 +1,34 @@
+########################################
+#
+# Chord Node - Quick Chord Simulation
+# Written By: Jason Lukose and Hari N. G
+#
+########################################
+
+class ChordNode:
+	# Constructor
+	def __init__(self, id):
+		self.id = id
+		self.successor = []
+		self.finger_table = {}
+		self.keys = []
+
+## FUNCTIONS OF NODE HERE
+	def create(self):
+		return None	
+
+	def join(self, JoinNode):
+		return None
+
+	def stabilize(self):
+		return None
+
+	def notify(self, NotifyNode):
+		return None
+
+	def fix_fingers(self):
+		return None
+
+
+
+
