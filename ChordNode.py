0202bccf272b4836341a7a95b894eb3656899f24
Ganapathy Hari Narayan <ganapathy.hari.narayan@gatech.edu>
2018-04-28 21:39:59 -0400

    Some stuff we are doing dont know

--- ChordNode.py
+++ ChordNode.py
@@ -19,24 +19,40 @@ class ChordNode:
 ## FUNCTIONS OF NODE HERE
     def create(self):
         self.predecessor = None
-        successor = self
+        self.successor = self
         return None 
 
     def join(self, randNode):
         self.predecessor = None
+        print("JOINING NODE IS" + self.id + "RAND NODE IS " + randNode.id)
         self.successor = randNode.find_successor(self.id)
+        print("Sucessor is now " + self.successor.id + "\n")
         return None
 
     def stabilize(self):
+        print("STABILIZING NODE " + self.id)
+        
         x = self.successor.predecessor
-        if x.id > self.id and x.id < self.successor.id:
+        if x != None:
+            print("BEFORE: " + self.id + " " + x.id)
+        if x != None and x.id > self.successor.id and x != self:
+            self.successor = x
+        if x != None and x.id > self.id and x.id <= self.successor.id:
             self.successor = x
         self.successor.notify(self)
-        return None
 
     def notify(self, notifyNode):
-        if (self.predecessor == None or (notifyNode.id > self.predecessor.id and notifyNode.id < self.id)):
+        if notifyNode.id > notifyNode.successor.id:
             self.predecessor = notifyNode
+        elif (self.predecessor == None or (notifyNode.id > self.predecessor.id and notifyNode.id < self.id)):
+            self.predecessor = notifyNode
+
+        print("Stab node's successor is " + notifyNode.successor.id)
+        if notifyNode.predecessor != None:
+            print("Stab node's predecessor is " + notifyNode.predecessor.id)
+        else:
+            print("Stab node's predecessor is None")
+
 
     def fix_fingers(self):
         return None
@@ -56,7 +72,7 @@ class ChordNode:
         return None
 
     def closest_predecessor(self, id):
-        for i in range(0, self.m - 1):
+        for i in range(0, ChordRing.m - 1):
             if (self.finger_table[m - i - 1].id >= self.id and 
                 self.finger_table[m - i - 1].id < id):
                 return self.finger_table[m - i - 1]
@@ -82,23 +98,32 @@ class ChordRing:
         return len(ChordRing.nodeList)
 
 def readLog():
-    print("hello");
     f = open("log_file.txt", "r")
+    print(ChordRing.m)
     for line in f:
-        command = line.split(" ")
+        command = line.rstrip().split(" ")
         if command[0] == "CREATE_NODE":
             node = ChordNode(command[1])
-            ChordRing.addNode(node)
             node.create()
+            ChordRing.addNode(node)
         elif command[0] == "INSERT_NODE":
             node = ChordNode(command[1])
-            ChordRing.addNode(node)
-            print(random.randint(0, ChordRing.getNumNodes()))
             node.join(ChordRing.getNodes()[random.randint(0, ChordRing.getNumNodes() - 1)])
+            ChordRing.addNode(node)
         elif command[0] == "INSERT_KEY":
             return None
         else:
             return None
+
+        for node in ChordRing.getNodes():
+            node.stabilize()
+
+        for node in ChordRing.getNodes():
+            node.stabilize()
+
+    ## 23 -> 28 -> 45 -> 23
+    for node in ChordRing.getNodes():
+        print(node.predecessor.id + " -> " + node.id + " -> " + node.successor.id)
 
 
 if __name__ == "__main__":
